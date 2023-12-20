cf9b4ea7ec7ab94549237796541acfa234cf1c60
James Niyongira <lascasasjames85@gmail.com>
2023-12-20 18:12:50 -0500

    added gitignore and hidden secret key in env vars

--- drf_ecomerce/settings/base.py
+++ drf_ecomerce/settings/base.py
@@ -2,7 +2,9 @@
 Django settings for drf_ecomerce project.
 
 """
+import os
 
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -10,12 +12,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
+load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4z$3*-344xvgax--ld+n1zjo8*(vjr#=^=*k^l3b_d#ep_ul=3'
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Application definition
 
