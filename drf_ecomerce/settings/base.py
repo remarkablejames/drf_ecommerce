b538bc5e0c19ac9d1d353175fcdfc10dbb5afe8f
James Niyongira <lascasasjames85@gmail.com>
2023-12-20 18:31:12 -0500

    added pytest configuration

--- drf_ecomerce/settings/base.py
+++ drf_ecomerce/settings/base.py
@@ -27,6 +27,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #     EXTERNAL APPS
+    'rest_framework',
+    #     LOCAL APPS
 ]
 
 MIDDLEWARE = [
@@ -98,3 +101,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+}
